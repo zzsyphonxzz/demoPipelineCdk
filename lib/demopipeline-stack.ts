6b85de111fc214c83e4ca939533064dc21613ad2
zzsyphonxzz <wayne.mahan@gmail.com>
2023-10-23 13:51:48 -0500

    remove unused imports

--- lib/demopipeline-stack.ts
+++ lib/demopipeline-stack.ts
@@ -1,15 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { PipelineAppStage } from './demoawspipeline-app-stack';
-import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
 
 export class DemopipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-   
-    const democicdpipeline = new CodePipeline(this,'demopipeline', {
+
+    const democicdpipeline = new CodePipeline(this, 'demopipeline', {
       synth: new ShellStep('Synth', {
         // Use a connection created using the AWS console to authenticate to GitHub
         // Other sources are available.
