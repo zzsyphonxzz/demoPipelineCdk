975f2951938173f638e26116ddc7309e9b6fab10
zzsyphonxzz <wayne.mahan@gmail.com>
2023-10-23 13:53:19 -0500

    fix typo

--- lib/demopipeline-stack.ts
+++ lib/demopipeline-stack.ts
@@ -11,7 +11,7 @@ export class DemopipelineStack extends cdk.Stack {
       synth: new ShellStep('Synth', {
         // Use a connection created using the AWS console to authenticate to GitHub
         // Other sources are available.
-        input: CodePipelineSource.gitHub('zzsyphonxzz/demoPipelineCdk/', 'main'),
+        input: CodePipelineSource.gitHub('zzsyphonxzz/demoPipelineCdk', 'main'),
         commands: [
           'npm ci',
           'npm run build',
